e21b8d43d162b0173f62675a6a4f39d700d5b096
Amenda <amenda@Catmendas-MacBook-Air.local>
2018-04-16 20:08:45 -0700

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+    has_secure_password
+    before_save :downcase_email
+    EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+    validates :name, presence: true
+    validates :password, presence: true
+
+    private
+       def downcase_email
+         self.email.downcase!
+       end
+
+end
