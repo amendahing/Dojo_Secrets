0f91fd3f8813f36abf741344a9bae7b9972ca70f
Amenda <amenda@Catmendas-MacBook-Air.local>
2018-04-17 18:26:07 -0700

    added likes and secrets models + testing for both

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+    has_many :secrets
+    has_many :likes, dependent: :destroy
+    has_many :secrets_liked, through: :likes, source: :secret
+
     has_secure_password
     before_save :downcase_email
     EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
